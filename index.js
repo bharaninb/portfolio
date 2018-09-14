89221744e84b8725a934f56a958067d516828a98
Bharani <bharanidharan.nb@gmail.com>
2018-09-14 20:21:02 +0530

    Init React app

---
+++ index.js
@@ -0,0 +1,15 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const element = (
+    <div>
+    <h1>I'm Bharanidharan</h1>
+    <h4>Full Stack Javascript Developer</h4>
+    </div>
+);
+
+ReactDOM.render(
+    element,
+    document.getElementById("root")
+);
